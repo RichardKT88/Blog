fab730b6957c3ba7979d1021d0440f0346a1ded5
Richard Kendy Tanaka <kendy_t88@hotmail.com>
2022-01-24 20:53:04 -0300

    3.6 - Listando banco de dados

--- Blog/Controllers/PostController.cs
+++ Blog/Controllers/PostController.cs
@@ -5,17 +5,32 @@ namespace Blog.Controllers
 {
     public class PostController : Controller
     {
-        public IActionResult Index()
+        private IList<Post> _list;
+        public PostController()
         {
-            var listaDePosts = new List<Post>()
+            this._list = new List<Post>()
             {
                 new Post(){Titulo = "Harry Potter 1", Resumo = "Pedra Filosofal", Categoria = "Filme e Livro"},
                 new Post(){Titulo = "Vigadores ", Resumo = "Ultimato", Categoria = "Filme"},
                 new Post(){Titulo = "Dom Casmurro", Resumo = "Romance sobre a dúvida", Categoria = "Livro"},
                 new Post(){Titulo = "Segredinho", Resumo = "Funk do bom!!!", Categoria = "Música"}
             };
-            //ViewBag.Posts = listaDePosts;
-            return View(listaDePosts);
+        }
+        public IActionResult Index()
+        {
+            
+            return View(_list);
+        }
+
+        public IActionResult Novo()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Adiciona(Post post)
+        {
+            _list.Add(post);
+            return View("Index", _list);
         }
     }
 }
---
+++ Blog/Infra/ConnectionFactory.cs
@@ -0,0 +1,20 @@
+﻿using System.Data.SqlClient;
+
+namespace Blog.Infra
+{
+    public class ConnectionFactory
+    {
+        public static SqlConnection CriaConexaoAberta()
+        {
+            var builder = new ConfigurationBuilder()
+                    .SetBasePath(Directory.GetCurrentDirectory())
+                    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                    .AddEnvironmentVariables();
+            IConfiguration configuration = builder.Build();
+            string stringConexao = configuration.GetConnectionString("Blog");
+            SqlConnection conexao = new SqlConnection(stringConexao);
+            conexao.Open();
+            return conexao;
+        }
+    }
+}
