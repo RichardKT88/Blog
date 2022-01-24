4ab89e0afb15b914819fbd5ebb29cb2edaa7503d
RICHARD TANAKA <richard.tanaka@viavarejo.com.br>
2022-01-24 17:59:25 -0300

    Firs Commit

---
+++ Blog/Controllers/PostController.cs
@@ -0,0 +1,21 @@
+﻿using Blog.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Blog.Controllers
+{
+    public class PostController : Controller
+    {
+        public IActionResult Index()
+        {
+            var listaDePosts = new List<Post>()
+            {
+                new Post(){Titulo = "Harry Potter 1", Resumo = "Pedra Filosofal", Categoria = "Filme e Livro"},
+                new Post(){Titulo = "Vigadores ", Resumo = "Ultimato", Categoria = "Filme"},
+                new Post(){Titulo = "Dom Casmurro", Resumo = "Romance sobre a dúvida", Categoria = "Livro"},
+                new Post(){Titulo = "Segredinho", Resumo = "Funk do bom!!!", Categoria = "Música"}
+            };
+            //ViewBag.Posts = listaDePosts;
+            return View(listaDePosts);
+        }
+    }
+}
