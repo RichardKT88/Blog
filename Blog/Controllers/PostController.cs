187db0764f8088fc20d466d1081949e25bf88873
Richard Kendy Tanaka <kendy_t88@hotmail.com>
2022-01-26 20:59:45 -0300

    Fim do Capítulo 3 - Persistindo posts em um banco de dados

--- Blog/Controllers/PostController.cs
+++ Blog/Controllers/PostController.cs
@@ -1,25 +1,21 @@
-﻿using Blog.Models;
+﻿using Blog.DAO;
+using Blog.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Blog.Controllers
 {
     public class PostController : Controller
     {
-        private IList<Post> _list;
+        
         public PostController()
         {
-            this._list = new List<Post>()
-            {
-                new Post(){Titulo = "Harry Potter 1", Resumo = "Pedra Filosofal", Categoria = "Filme e Livro"},
-                new Post(){Titulo = "Vigadores ", Resumo = "Ultimato", Categoria = "Filme"},
-                new Post(){Titulo = "Dom Casmurro", Resumo = "Romance sobre a dúvida", Categoria = "Livro"},
-                new Post(){Titulo = "Segredinho", Resumo = "Funk do bom!!!", Categoria = "Música"}
-            };
+            
         }
         public IActionResult Index()
         {
-            
-            return View(_list);
+            PostDAO dao = new PostDAO();
+            IList<Post> lista = dao.Lista();
+            return View(lista);
         }
 
         public IActionResult Novo()
@@ -29,8 +25,9 @@ namespace Blog.Controllers
         [HttpPost]
         public IActionResult Adiciona(Post post)
         {
-            _list.Add(post);
-            return View("Index", _list);
+            PostDAO dao = new PostDAO();
+            dao.Adiciona(post);
+            return RedirectToAction("Index");
         }
     }
 }
---
+++ Blog/DAO/PostDAO.cs
@@ -0,0 +1,45 @@
+﻿using Blog.Infra;
+using Blog.Models;
+using System.Data.SqlClient;
+
+namespace Blog.DAO
+{
+    public class PostDAO
+    {
+        public IList<Post> Lista() 
+        {
+            var lista = new List<Post>();
+            using (SqlConnection cnx = ConnectionFactory.CriaConexaoAberta()) 
+            {
+                SqlCommand comando = cnx.CreateCommand();
+                comando.CommandText = "select * from Posts";
+                SqlDataReader leitor = comando.ExecuteReader();
+                while (leitor.Read())
+                {
+                    Post post = new Post()
+                    {
+                        Id = Convert.ToInt32(leitor["id"]),
+                        Titulo = Convert.ToString(leitor["titulo"]),
+                        Resumo = Convert.ToString(leitor["resumo"]),
+                        Categoria = Convert.ToString(leitor["categoria"]),
+                    };
+                    lista.Add(post);
+                }
+            }
+            return lista;
+        }
+        public void Adiciona(Post post) 
+        {
+            using (SqlConnection cnx = ConnectionFactory.CriaConexaoAberta()) 
+            {
+                SqlCommand comando = cnx.CreateCommand();
+                comando.CommandText = "insert into Posts (Titulo, Resumo, Categoria) values (@titulo, @resumo, @categoria)";
+                comando.Parameters.Add(new SqlParameter("titulo", post.Titulo));
+                comando.Parameters.Add(new SqlParameter("resumo", post.Resumo));
+                comando.Parameters.Add(new SqlParameter("categoria", post.Categoria));
+                comando.ExecuteNonQuery();
+            }
+            
+        }
+    }
+}
